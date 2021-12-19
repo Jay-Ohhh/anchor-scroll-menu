0f9cf84c9bd435fbca396fc6197d6defdc91cb36
Jay <1598353326@qq.com>
2021-12-19 17:48:44 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'anchor-scroll-menu',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+
+  // 配置具体含义见：https://github.com/umijs/umi-webpack-bundle-analyzer#options-for-plugin
+  analyze: {
+    analyzerMode: 'server',
+    analyzerPort: 8888,
+    openAnalyzer: true,
+    // generate stats file while ANALYZE_DUMP exist
+    generateStatsFile: false,
+    statsFilename: 'stats.json',
+    logLevel: 'info',
+    defaultSizes: 'parsed', // stat  // gzip
+  },
+});
